fa6a57da94972bb64bfa48960eefbcefc25d6ac3
SixStringsCoder <rshanlon@gmail.com>
2017-09-28 22:07:14 -0700

    Develop minesweeper to generate game board and bomb board using FOR and nested FOR loops

--- minesweeper/lib/minesweeper.js
+++ minesweeper/lib/minesweeper.js
@@ -5,18 +5,63 @@ This project programs a Minesweeper game
 created by Steve Hanlon Sept 21, 2017
 */
 
-// hard code a sample row of squares
-var board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+// Generate empty game board
+var generatePlayerBoard = function generatePlayerBoard(numberOfRows, numberOfColumns) {
+  var board = [];
 
-var printBoard = function printBoard(board) {
-  console.log('Current Board:');
-  console.log(board[0].join('|'));
-  console.log(board[1].join('|'));
-  console.log(board[2].join('|'));
+  for (var rows = 0; rows < numberOfRows; rows++) {
+    // represents a single row to be added to the game board.
+    var row = [];
+    for (var cols = 0; cols < numberOfColumns; cols++) {
+      // generates a space for a column
+      row.push(' ');
+    }
+    // generates rows with the number of columns indicated above
+    board.push(row);
+  }
+  return board;
 };
 
-printBoard(board);
+// Generate Bomb board
+var generateBombBoard = function generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs) {
+  var board = [];
 
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+  for (var rows = 0; rows < numberOfRows; rows++) {
+    // represents a single row to be added to the game board.
+    var row = [];
+    for (var cols = 0; cols < numberOfColumns; cols++) {
+      // generates a space for a column
+      row.push(null);
+    }
+    // generates rows with the number of columns indicated above
+    board.push(row);
+  }
+  var numberOfBombsPlaced = 0;
+  //add bombs to the board until our counter reaches the specified number of bombs
+  while (numberOfBombsPlaced < numberOfBombs) {
+    var randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    var randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    // ***has the potential to place bombs on top of already existing bombs.
+    board[randomRowIndex][numberOfColumns] = 'B';
+    numberOfBombsPlaced += 1;
+  }
+
+  return board;
+};
+
+// Print Game
+var printBoard = function printBoard(board) {
+  // Adds a pipe joiner between spaces
+  // Joins all rows into a string and moves each successive row to a new line
+  console.log(board.map(function (row) {
+    return row.join(' | ');
+  }).join('\n'));
+};
+
+var playerBoard = generatePlayerBoard(3, 4);
+var bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,27 +1,65 @@
 /*
-This project programs a Minesweeper game
+This project programs a Minesweeper game using ES6 and Babel transpiler
 created by Steve Hanlon Sept 21, 2017
 */
 
-// Rows of the game board
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+// Generate empty game board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
 
-// Print array and join them with a pipe to complete an empty board
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join('|'));
-  console.log(board[1].join('|'));
-  console.log(board[2].join('|'));
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    // represents a single row to be added to the game board.
+    const row = [];
+    for (let cols = 0; cols < numberOfColumns; cols++) {
+      // generates a space for a column
+      row.push(' ');
+    }
+    // generates rows with the number of columns indicated above
+    board.push(row);
+  }
+    return board;
 }
 
-// View sample empty board
-printBoard(board);
 
-// Hard code a sample guess and bomb for comparison
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+// Generate Bomb board
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+
+  for (let rows = 0; rows < numberOfRows; rows++) {
+    // represents a single row to be added to the game board.
+    const row = [];
+    for (let cols = 0; cols < numberOfColumns; cols++) {
+      // generates a space for a column
+      row.push(null);
+    }
+    // generates rows with the number of columns indicated above
+    board.push(row);
+  }
+    // counter
+    let numberOfBombsPlaced = 0;
+    //add bombs to the board until our counter reaches the specified number of bombs
+    while(numberOfBombsPlaced !== numberOfBombs) {
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      // ***has the potential to place bombs on top of already existing bombs.
+      board[randomRowIndex][numberOfColumns] = 'B';
+      numberOfBombsPlaced += 1;
+    }
+    return board;
+}
+
+// Print Game
+const printBoard = (board) => {
+  /* Adds a pipe joiner between spaces,
+  joins all rows into a string and moves each successive row to a new line */
+  console.log(board.map((row) => row.join(' | ')).join('\n'));
+}
+
+// sample bomb board
+const playerBoard = generatePlayerBoard(3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
