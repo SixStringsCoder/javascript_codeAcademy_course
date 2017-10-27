36fc15f9d71c6c186384ccfd0fa80dcae98a56dc
SixStringsCoder <rshanlon@gmail.com>
2017-10-26 19:32:52 -0700

    Added visual feedback for the sorting options at the top of the search bar;Set the state of sorting options and input elements;Simulated a search query with the 'Let's Go' button

--- React_projects/ravenous/src/components/SearchBar/SearchBar.js
+++ React_projects/ravenous/src/components/SearchBar/SearchBar.js
@@ -6,17 +6,66 @@ import './SearchBar.css'
 const sortByOptions = {
   'Best Match': 'best_match',
   'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-}
+  'Most Reviewed': 'review_count',
+};
 
 class SearchBar extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        term: '', // search term located in the search input (e.g. Pizza, Noodles)
+        location: '', //location entered in the search input
+        sortBy: 'best_match'
+      };
+      this.handleTermChange = this.handleTermChange.bind(this);
+      this.handleLocationChange = this.handleLocationChange.bind(this);
+      this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    // returns the current CSS class 'active' for a the sortBy option, highlighting selected menu item in tan
+    getSortByClass(sortByOption) {
+      if (this.state.sortBy === sortByOption) {
+        return 'active';
+      } else {
+        return '';
+      }
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({
+          sortBy: sortByOption
+        });
+    }
+
+    // reflect the text typed into the search Term input element -- "Search Businesses"
+    handleTermChange(event) {
+        this.setState({
+          term: event.target.value,
+        });
+    }
+
+    // reflect the text typed into the search Location input element -- "Where"
+    handleLocationChange(event) {
+        this.setState({
+          location: event.target.value,
+        });
+    }
+
+    handleSearch(event) {
+      this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+      // prevent the default action of clicking a link from triggering at the end of the method.
+      event.preventDefault();
+    }
+
   /* Dynamically create the list items needed to display
   the sort options (Best Match, Highest Rated, Most Reviewed).*/
   renderSortByOptions() {
     // loop through key and map values, attach a key attribute
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                className={this.getSortByClass(sortByOptionValue)}
+                key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
 
@@ -29,11 +78,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a href="localhost:3000">Lets Go</a>
+          <a onClick={this.handleSearch} href="localhost:3000">Lets Go</a>
         </div>
       </div>
     );
