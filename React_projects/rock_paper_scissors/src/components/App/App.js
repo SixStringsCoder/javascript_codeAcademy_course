34b8ce791a661c3c1aed8d372055a473f4194bed
SixStringsCoder <rshanlon@gmail.com>
2017-12-02 18:33:22 -0800

    Refactored whoWins method and Referee callouts; Score now works with correct callout; Final Game Over message works; window reset works in setTimeout callback

--- React_projects/rock_paper_scissors/src/components/App/App.js
+++ React_projects/rock_paper_scissors/src/components/App/App.js
@@ -18,7 +18,6 @@ class App extends Component {
     this.setCompScore = this.setCompScore.bind(this);
     this.refereeCall = this.refereeCall.bind(this);
     this.whoWins = this.whoWins.bind(this);
-    this.resetScoreBoard = this.resetScoreBoard.bind(this);
   }
 
   // Choose an answer
@@ -51,76 +50,87 @@ class App extends Component {
   }
 
   setYourScore() {
-    this.setState({
-      yourScore: this.state.yourScore + 1,
-    });
-    console.log(`You win that round! You: ${this.state.yourScore} Compcrusher: ${this.state.compScore}`);
+    // If You get last point,
+    if (this.state.yourScore === 2) {
+      // Increment score
+      this.setState({
+        yourScore: this.state.yourScore + 1,
+      });
+      // Show "Game Over" message
+      this.refereeCall(4);
+      // Reset window which auto resets score
+      setTimeout(function(){ window.location.reload(true); }, 2500);
+    } else {
+      // Increment score before last point
+      this.setState({
+        yourScore: this.state.yourScore + 1,
+      });
+    }
   }
 
   setCompScore() {
-    this.setState({
-      compScore: this.state.compScore + 1,
-    });
-    console.log(`Comp wins that round! You: ${this.state.yourScore} Compcrusher: ${this.state.compScore}`);
+    // If computer gets last point,
+    if (this.state.compScore === 2) {
+      // Increment score
+      this.setState({
+        compScore: this.state.compScore + 1,
+      });
+      // Show "Game Over" message
+      this.refereeCall(5);
+      // Reset window which auto resets score
+      setTimeout(function(){ window.location.reload(true); }, 2500);
+    } else {
+      // Increment score before last point
+      this.setState({
+        compScore: this.state.compScore + 1,
+      });
+    }
   }
-
-  resetScoreBoard() {
-    // After a match winner, reset page
-    window.location.reload(true);
-    // reset scoreboard
-    this.setState({
-      yourScore: 0,
-      compScore: 0
-    });
-  }
-
 
 
   // These are the short phrases populating center screen between plays to indicate a tie or who won
   refereeCall(callOut) {
     const refCall = [
       "Tie!",
-      "You got that one!",
-      "Compcrusher got that one!",
-      "Game over! You win!",
-      "Game over! Compcrusher wins!"
+      "Rock breaks scissors",
+      "Paper covers rock",
+      "Scissors cut paper",
+      "GAME OVER! You win!",
+      "GAME OVER! Compcrusher wins!"
     ];
-    console.log(`${callOut} returns ${refCall[callOut]}`);
+
     this.setState({
       referee: refCall[callOut]
     });
   }
 
+
   whoWins(yourChoice, compChoice) {
-    // yourChoice and compChoice return numbers 0 to 2
-    // It's a tie
-    if (yourChoice === compChoice) {
-      this.refereeCall(0);
-      console.log(`Tie! You: ${this.state.yourScore} Compcrusher: ${this.state.compScore}`);
-      // You win that round
-    } else if (yourChoice === 0 && compChoice === 2 ||
-    yourChoice === 1 && compChoice === 0 ||
-    yourChoice === 2 && compChoice === 1){
-      // You get a point
-      this.setYourScore();
-      this.refereeCall(1);
-    } else {
-      // Computer wins that round and gets a point
-      this.setCompScore();
-      this.refereeCall(2);
+    // All outcomes before game ends at 3
+    if (this.state.yourScore != 3 || this.state.compScore != 3) {
+        if (yourChoice === 0 && compChoice === 2) {
+          this.refereeCall(1);
+          this.setYourScore();
+        } else if (yourChoice === 1 && compChoice === 0 ) {
+          this.refereeCall(2);
+          this.setYourScore();
+        } else if (yourChoice === 2 && compChoice === 1) {
+          this.refereeCall(3);
+          this.setYourScore();
+        } else if (yourChoice === 2 && compChoice === 0) {
+          this.refereeCall(1);
+          this.setCompScore();
+        } else if (yourChoice === 0 && compChoice === 1 ) {
+          this.refereeCall(2);
+          this.setCompScore();
+        } else if (yourChoice === 1 && compChoice === 2) {
+          this.refereeCall(3);
+          this.setCompScore();
+        } else {
+          this.refereeCall(0);
+        }
+      };
     }
-    // Conditions to win the game
-    if (this.state.yourScore === 3) {
-      this.refereeCall(3);
-      setTimeout(this.resetScoreBoard().bind(this), 3000);
-      return false;
-    } else if (this.state.compScore === 3) {
-      this.refereeCall(4);
-      setTimeout(this.resetScoreBoard().bind(this), 3000);
-    } else {
-      return false;
-    }
-  }
 
   render() {
     return (
