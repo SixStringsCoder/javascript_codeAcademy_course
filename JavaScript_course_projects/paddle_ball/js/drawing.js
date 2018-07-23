0d5cb50c5098cc50a3da9f40563a10df9f1f3401
SixStringsCoder <rshanlon@gmail.com>
2018-07-22 20:10:28 -0700

    Add functions to move paddle on arrow key strokes

--- JavaScript_course_projects/paddle_ball/js/drawing.js
+++ JavaScript_course_projects/paddle_ball/js/drawing.js
@@ -4,6 +4,11 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 const ballRadius = 10;
+const paddleHeight = 20;
+const paddleWidth = 70;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
 let ballColor = "rgba(0, 149, 221, 1)";
 let x_pos = canvas.width / 2;
 let y_pos = canvas.height - 30;
@@ -17,9 +22,21 @@ function changeColors() {
     ballColor = `rgba(${red}, ${green}, ${blue}, 1)`;
 }
 
-function drawBall() {
-    "use strict";
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red"
+    ctx.fill()
+    ctx.closePath();
 
+    if ( rightPressed && paddleX < (canvas.width - paddleX) ) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+}
+
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x_pos, y_pos, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = ballColor;
@@ -28,9 +45,8 @@ function drawBall() {
 }
 
 function draw() {
-    "use strict";
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
     drawBall();
 
     // side walls bounce
@@ -45,6 +61,29 @@ function draw() {
     }
     x_pos += draw_x;
     y_pos += draw_y;
+
+    drawPaddle();
 }
+
+function keyDownHandler(event) {
+    console.log(event);
+    if (event.keyCode === 39) {
+        rightPressed = true;
+    } else if (event.keyCode === 37) {
+       leftPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    console.log(event);
+    if (event.keyCode === 39) {
+        rightPressed = false;
+    } else if (event.keyCode === 37) {
+       leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 setInterval(draw, 10);
